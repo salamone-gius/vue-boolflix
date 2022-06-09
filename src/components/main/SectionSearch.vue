cbe593860122e9ba70c9a461b4c712b7d520e0f7
salamone-gius <salamone.gius@gmail.com>
2022-06-09 18:56:32 +0200

    axios imported and get to created with params

--- src/components/main/SectionSearch.vue
+++ src/components/main/SectionSearch.vue
@@ -5,6 +5,7 @@
 </template>
 
 <script>
+import axios from 'axios';
 import SearchBar from '../commons/SearchBar.vue';
 
 export default {
@@ -27,6 +28,21 @@ export default {
         searchSomething(searchThis) {
             console.log(searchThis);
         }
+    },
+
+    created() {
+        axios.get('https://api.themoviedb.org/3/search/movie', {
+            params: {
+                api_key: '88298d08dd2155d218d8febb493ff902',
+                // testo da cercare (dinamico con v-model)
+                query: 'ritorno',
+                language: 'it-IT'
+            }
+        }).then((response) => {
+            console.log(response.data.results);
+        }).catch((error) => {
+            console.log(error);
+        })
     }
 }
 </script>
