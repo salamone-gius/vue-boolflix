56699b039127981adfbdd56b7f5808781086d1f4
salamone-gius <salamone.gius@gmail.com>
2022-06-09 19:18:21 +0200

    commented SearchBar component (fail, for now)

--- src/components/main/SectionSearch.vue
+++ src/components/main/SectionSearch.vue
@@ -1,35 +1,41 @@
 <template>
     <section class="search container">
-        <SearchBar @searching="searchSomething" :inputPlaceholder="searchbarInfo.placeholder" :buttonText="searchbarInfo.button"/>
+        <!-- <SearchBar v-model="cerca" @searching="searchSomething" :inputPlaceholder="searchbarInfo.placeholder" :buttonText="searchbarInfo.button"/> -->
+        <ul>
+            <li v-for="movie in movies" :key="movie.id">{{movie.title}}</li>
+        </ul>
     </section>
 </template>
 
 <script>
 import axios from 'axios';
-import SearchBar from '../commons/SearchBar.vue';
+// import SearchBar from '../commons/SearchBar.vue';
 
 export default {
     name: 'SectionSearch',
 
     components: {
-        SearchBar,
+        // SearchBar,
     },
 
     data() {
         return {
-            searchbarInfo: {
-                placeholder: "Search movie, tv series and more",
-                button: "Search",
-            }
+            // searchbarInfo: {
+            //     placeholder: "Search movie, tv series and more",
+            //     button: "Search",
+            // },
+
+            cerca: "",
+
+            movies: [],
         }
     },
 
     methods: {
-        searchSomething(searchThis) {
-            console.log(searchThis);
-        }
+        // searchSomething(searchThis) {
+        //     console.log(searchThis);
     },
-
+    
     created() {
         axios.get('https://api.themoviedb.org/3/search/movie', {
             params: {
@@ -39,7 +45,9 @@ export default {
                 language: 'it-IT'
             }
         }).then((response) => {
-            console.log(response.data.results);
+            console.log("response.data.results = ", response.data.results);
+            this.movies = response.data.results;
+            console.log("array movies = ", this.movies);
         }).catch((error) => {
             console.log(error);
         })
