84f6d2bcb58d37838e183a779173aa90213f8431
salamone-gius <salamone.gius@gmail.com>
2022-06-09 19:37:46 +0200

    request to server at submit event

--- src/components/main/SectionSearch.vue
+++ src/components/main/SectionSearch.vue
@@ -1,6 +1,11 @@
 <template>
     <section class="search container">
         <!-- <SearchBar v-model="cerca" @searching="searchSomething" :inputPlaceholder="searchbarInfo.placeholder" :buttonText="searchbarInfo.button"/> -->
+        <form class="input-group my-3" @submit.prevent="searchSomething">
+            <input v-model="searchText" placeholder="Search" type="text" class="form-control" aria-label="Search" aria-describedby="button-addon2">
+            <button class="btn btn-outline-secondary" type="button" id="button-addon2">Search</button>
+        </form>
+
         <ul>
             <li v-for="movie in movies" :key="movie.id">{{movie.title}}</li>
         </ul>
@@ -25,33 +30,30 @@ export default {
             //     button: "Search",
             // },
 
-            cerca: "",
+            searchText: "",
 
             movies: [],
         }
     },
 
     methods: {
-        // searchSomething(searchThis) {
-        //     console.log(searchThis);
+        searchSomething() {
+            axios.get('https://api.themoviedb.org/3/search/movie', {
+                params: {
+                    api_key: '88298d08dd2155d218d8febb493ff902',
+                    // testo da cercare (dinamico con v-model)
+                    query: this.searchText,
+                    language: 'it-IT'
+                }
+            }).then((response) => {
+                console.log("response.data.results = ", response.data.results);
+                this.movies = response.data.results;
+                console.log("array movies = ", this.movies);
+            }).catch((error) => {
+                console.log(error);
+            })
+        }
     },
-    
-    created() {
-        axios.get('https://api.themoviedb.org/3/search/movie', {
-            params: {
-                api_key: '88298d08dd2155d218d8febb493ff902',
-                // testo da cercare (dinamico con v-model)
-                query: 'ritorno',
-                language: 'it-IT'
-            }
-        }).then((response) => {
-            console.log("response.data.results = ", response.data.results);
-            this.movies = response.data.results;
-            console.log("array movies = ", this.movies);
-        }).catch((error) => {
-            console.log(error);
-        })
-    }
 }
 </script>
 
