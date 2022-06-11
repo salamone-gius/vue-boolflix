14cecdb26d6f6d824b685f58cea74ac14a0a5758
salamone-gius <salamone.gius@gmail.com>
2022-06-11 13:42:33 +0200

    movies info printed on SectionSearch by globalObj

--- src/components/main/SectionSearch.vue
+++ src/components/main/SectionSearch.vue
@@ -1,15 +1,15 @@
 <template>
     <section class="search container">
-        <!-- <ul>
-            <li v-for="movie in movies" :key="movie.id">
+        <ul>
+            <li v-for="movie in globalObject.movies" :key="movie.id">
                 <h3>{{movie.title}}</h3>
                 <ul>
                     <li>Titolo originale: {{movie.original_title}}</li>
                     <li>Lingua: {{movie.original_language}}</li>
-                    <li>Voto: {{movie.popularity}}</li>
+                    <li>Voto: {{movie.vote_average}}</li>
                 </ul>
             </li>
-        </ul> -->
+        </ul>
     </section>
 </template>
 
